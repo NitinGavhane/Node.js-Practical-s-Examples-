4f7122a9425e2dee9ed4f077991cc8d0758cad1d
Nitin Gavhane <nitin.gavhane@example.com>
2025-08-24 03:32:14 +0530

    4th exampel to create custom function with middleware and Send Post request from html to get

--- 4.Middleware/index4.js
+++ 4.Middleware/index4.js
@@ -1,7 +1,34 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { log } from "console";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var GenerateID = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html"); 
+});
+
+function GenerateIDfunction(req, res, next) {
+  console.log("GenerateID");
+  GenerateID = req.body.name + req.body.email;
+    next();
+}
+
+app.use(GenerateIDfunction);
+
+app.post("/submit", (req, res, next) => {
+  console.log(GenerateID);
+  res.send(`<h1>Your ID is:</h1><h2>${GenerateID}✌️</h2>`);
+
+});
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
